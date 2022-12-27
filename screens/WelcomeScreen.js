066dde266c7299d118ee57dee4ae10150641c7c9
Yanters <yanter@wp.pl>
2022-12-27 17:11:07 +0100

    saving and sending token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,29 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState(null);
+
+  const authCtx = useContext(AuthContext);
+  const { token } = authCtx;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://react-native-b6ece-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=${token}`
+      )
+      .then((response) => {
+        setMessage(response.data);
+      });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
